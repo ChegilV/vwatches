7e50675b4331108c4efd2263116f179901211879
VChegil <vovachegil@gmail.com>
2023-09-23 22:05:51 +0300

    new allowed host

--- puddle/settings_rail.py
+++ puddle/settings_rail.py
@@ -3,7 +3,7 @@ from decouple import config
 
 
 SECRETE_KEY = config('SECRETE_KEY')
-ALLOWED_HOSTS = ['web-production-8000.up.railway.app']
+ALLOWED_HOSTS = ['vwatches-chegil.up.railway.app']
 
 DATABASES = {
     'default': {
