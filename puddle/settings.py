018047982b2cc01dc7855168fd0f83fa88bc6df6
ChegilV <132985611+ChegilV@users.noreply.github.com>
2023-12-27 20:31:13 +0200

    Update settings.py

--- puddle/settings.py
+++ puddle/settings.py
@@ -85,19 +85,7 @@ WSGI_APPLICATION = 'puddle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': 'JtSPKODlqd5wSO11ktTa',
-#         'HOST': 'containers-us-west-139.railway.app',
-#         'PORT': '7386',
-#     }
-# }
-# import dj_database_url
-# dj_form_env = dj_database_url.config(conn_max_age=600)
-# DATABASES.['default'].update(dj_form_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
